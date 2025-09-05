c71e71e64615ee364912646c2d12e634a8b78e75
Kayke Queiroz <kayke.queiroz@ucsal.edu.br>
2025-09-04 23:38:40 -0300

    Resolvido

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,11 +1,17 @@
 package br.com.mariojp.solid.ocp;
 
+import java.util.Map;
+
 public class DiscountCalculator {
-    public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
-        }
+
+    private final Map<CustomerType, DiscountPolicy> policies;
+
+    public DiscountCalculator(Map<CustomerType, DiscountPolicy> policies) {
+        this.policies = policies;
+    }
+
+    public double apply(double amount, CustomerType type) {
+        DiscountPolicy policy = policies.getOrDefault(type, a -> a);
+        return policy.apply(amount);
     }
 }
